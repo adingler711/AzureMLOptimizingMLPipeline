864c421e48db59f95c028fe0afba1afb5984aa9c
Andy Dingler <andydingler711@gmail.com>
2021-02-21 21:11:06 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -14,14 +14,7 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = TabularDatasetFactory.from_delimited_files(["https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"])
-
-x, y = clean_data(ds)
-
 # TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=7)
 
 run = Run.get_context()
 
@@ -53,6 +46,11 @@ def clean_data(data):
     
 
 def main():
+    ds = TabularDatasetFactory.from_delimited_files(
+        ["https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"])
+    x, y = clean_data(ds)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=7)
+    
     # Add arguments to script
     parser = argparse.ArgumentParser()
 
